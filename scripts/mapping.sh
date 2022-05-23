8905fdf246f81bc017f26af37040e5f1f034134a
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2022-05-23 19:08:14 +0800

    Update mapping.sh

--- scripts/mapping.sh
+++ scripts/mapping.sh
@@ -1,7 +1,7 @@
 name=b48h1-mht
-
+software_path="/software"
 run_path=/project/$name
 
 db="/database/Macaca_fascicularis/NCBI/GCF_000364345.1/all.fa"
 
-/zfswh1/BC_RD_P0/heyingdong/project/IsoSeq_SingelCell/ProjectMouseSperm/software/minimap2/minimap2 -ax splice -t 16 -uf --secondary=no -C5 $db  ${run_path}/FL/fl_seq.fq > ${run_path}/FL/fl_seq.sam
+${software_path}/minimap2 -ax splice -t 16 -uf --secondary=no -C5 $db  ${run_path}/FL/fl_seq.fq > ${run_path}/FL/fl_seq.sam
